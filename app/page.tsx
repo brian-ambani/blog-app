253d48ac5b032c5f00d38996ce19145bf19b01ed
Brian-Ambani <brainambani1@gmail.com>
2024-02-25 14:35:57 +0300

    add sanity to the client

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,35 @@
+import { Card } from "@/components/ui/card";
+import { simpleBlogCard } from "./lib/interface";
+import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
-import Navbar from "./components/Navbar";
 
-export default function Home() {
+async function getData() {
+  const query = `*[_type == 'blog'] | order(_createdAt desc) {
+    title,
+      smallDescription,
+      "currentSlug": slug.current,
+      titleImage
+  }`;
+
+  const data = await client.fetch(query);
+  return data;
+}
+
+export default async function Home() {
+  const data: simpleBlogCard[] = await getData();
+
   return (
-    <div>
-      <Navbar />
-      <h1>Home</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+      {data.map((post, idx) => (
+        <Card key={idx}>
+          <Image
+            src={urlFor(post.titleImage).url()}
+            alt="image"
+            width={500}
+            height={500}
+          />
+        </Card>
+      ))}
     </div>
   );
 }
