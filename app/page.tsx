2d4f972a13f58f5a73b86d157fbd4b18a6937eff
Brian-Ambani <brainambani1@gmail.com>
2024-02-26 03:44:00 +0300

    completed the blog app

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { simpleBlogCard } from "./lib/interface";
 import { client, urlFor } from "./lib/sanity";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 async function getData() {
   const query = `*[_type == 'blog'] | order(_createdAt desc) {
@@ -19,7 +21,7 @@ export default async function Home() {
   const data: simpleBlogCard[] = await getData();
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-4 mt-5">
+    <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
       {data.map((post, idx) => (
         <Card key={idx}>
           <Image
@@ -27,7 +29,17 @@ export default async function Home() {
             alt="image"
             width={500}
             height={500}
+            className="rounded-t-lg h-[200px] object-cover"
           />
+          <CardContent className="mt-5">
+            <h3 className="text-lg line-clamp-2 font-bold">{post.title}</h3>
+            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+              {post.smallDescription}
+            </p>
+            <Button asChild className="w-full mt-7">
+              <Link href={`/blog/${post.currentSlug}`}>Read More</Link>
+            </Button>
+          </CardContent>
         </Card>
       ))}
     </div>
